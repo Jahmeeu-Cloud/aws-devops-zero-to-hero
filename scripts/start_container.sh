65697edfe4b8ba9485c03b963201607b61f44977
Jahmeeu-Cloud <jamiu.o.bakre@gmail.com>
2026-07-14 00:47:48 +0100

    add the new file code

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the docker image from Docker Hub
-docker pull horlacloud/simple-python-app:latest
+ docker pull horlacloud/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 horlacloud/simple-python-app:latest
+docker run -d -p 5000:5000 horlacloud/simple-python-app

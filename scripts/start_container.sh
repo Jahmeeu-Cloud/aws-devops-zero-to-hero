ad3e27144b447baf5a4fba5a03eb6e3c23666a07
Jahmeeu-Cloud <jamiu.o.bakre@gmail.com>
2026-07-13 20:04:56 +0100

    correct the start_container.sh code

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+# 1. Build the image with the specific tag
+# Replace 'latest' if you are using specific versioning
+docker build -t horlacloud/simple-python-app:latest .
+
 # Pull the Docker image from Docker Hub
-docker push horlacloud/simple-python-app:latest.
+docker push horlacloud/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 horlacloud/simple-python-app:latest.
+docker run -d -p 5000:5000 --name python-app horlacloud/simple-python-app:latest

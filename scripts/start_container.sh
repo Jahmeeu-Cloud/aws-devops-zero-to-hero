3e21845100be4735a49331411c33ba6b66e60e0a
Jahmeeu-Cloud <jamiu.o.bakre@gmail.com>
2026-07-13 20:18:47 +0100

    change to latest

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the docker image from Docker Hub
-docker pull horlacloud/simple-python-app:tagname
+docker pull horlacloud/simple-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 horlacloud/simple-python-app:tagname
+docker run -d -p 5000:5000 horlacloud/simple-python-app:tlatest

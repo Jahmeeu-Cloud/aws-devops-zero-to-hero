fde34d53f473cc86cd1e155c64849142c6ddb697
Jahmeeu-Cloud <jamiu.o.bakre@gmail.com>
2026-07-14 01:38:24 +0100

    Update script to stop/remove container on port 5000

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid='docker ps | awk -f  " " '{print $1}''docker rm-f
-docker rm -f $contianerid
+# Find any running container using port 5000 and stop/remove it
+container_id=$(docker ps -q --filter "publish=5000")
+
+if [ ! -z "$container_id" ]; then
+    echo "Stopping container on port 5000: $container_id"
+    docker stop $container_id
+    docker rm -f $container_id
+else
+    echo "No container is currently running on port 5000."
+fi
